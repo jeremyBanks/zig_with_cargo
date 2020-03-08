f5648823c0b3ef936786d19aa7b48a059751b0c4
Jeremy Banks <_@jeremy.ca>
2020-03-08 15:28:04 -0600

    testing allocator

--- src/ziggy.zig
+++ src/ziggy.zig
@@ -13,6 +13,30 @@ const BinaryTreeSet = struct {
         return tree;
     }
 
+    fn destroy(self: *BinaryTreeSet) void {
+        self.clear();
+        self.allocator.destroy(self);
+    }
+
+    fn clear(self: *BinaryTreeSet) void {
+        if (self.root) |root| {
+            self.destroy_node(root);
+            self.root = null;
+        }
+    }
+
+    fn destroy_node(self: *BinaryTreeSet, node: *BinaryTreeSetNode) void {
+        if (node.lesserChild) |lesserChild| {
+            self.destroy_node(lesserChild);
+        }
+
+        if (node.greaterChild) |greaterChild| {
+            self.destroy_node(greaterChild);
+        }
+
+        self.allocator.destroy(self);
+    }
+
     fn insert(self: *BinaryTreeSet, value: i64) !void {
         if (self.root) |root| {
             var parent = root;
@@ -80,6 +104,7 @@ fn main() !void {
     std.debug.warn("Let's make a binary tree.\n", .{});
 
     const tree = try BinaryTreeSet.create(std.heap.c_allocator);
+    defer tree.destroy();
 
     try tree.insert(12);
     try tree.insert(3);
@@ -92,6 +117,26 @@ fn main() !void {
     std.debug.warn("We did it! {}\n", .{tree});
 
     tree.for_each(print);
+}
+
+test "something" {
+    std.testing.expect(true);
+
+    const tree = try BinaryTreeSet.create(std.testing.allocator);
+    defer tree.destroy();
+
+    try tree.insert(12);
+    try tree.insert(3);
+    try tree.insert(61);
+    try tree.insert(59);
+    try tree.insert(2);
+    try tree.insert(5);
+    try tree.insert(5);
+    try tree.insert(5);
+
+    tree.for_each(print);
+
+    std.debug.warn("We did it! {}\n", .{tree});
 }
 
 fn print(x: i64) void {
